b1d94f8ab60eed6313226c54d8737d16bae27ec7
maconidreamer <Laurean2279@yandex.ru>
2026-06-20 09:55:14 +0300

    Add Opera class extending MusicalShow

--- Opera.java
+++ Opera.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
 
+public class Opera extends MusicalShow {
+    private final int choirSize;
+
+    public Opera(
+            String title,
+            int duration,
+            Director director,
+            ArrayList<Actor> listOfActors,
+            String musicAuthor,
+            String librettoText,
+            int choirSize
+    ) {
+        super(title, duration, director, listOfActors, musicAuthor, librettoText);
+        this.choirSize = choirSize;
+    }
+
+    public int getChoirSize() {
+        return choirSize;
+    }
+}
