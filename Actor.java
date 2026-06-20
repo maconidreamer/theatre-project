865242b351268ea24132964e00a858dc10383a43
maconidreamer <Laurean2279@yandex.ru>
2026-06-20 09:53:54 +0300

    Implement Actor class extending Person

--- Actor.java
+++ Actor.java
@@ -1,1 +1,41 @@
+import java.util.Objects;
 
+public class Actor extends Person {
+    private final int height;
+
+    public Actor(String name, String surname, Gender gender, int height) {
+        super(name, surname, gender);
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " " + getSurname() + " (" + height + " см)";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Actor actor = (Actor) obj;
+
+        return height == actor.height
+                && getName().equals(actor.getName())
+                && getSurname().equals(actor.getSurname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname(), height);
+    }
+}
