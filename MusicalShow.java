76802bad0184121ef774f515c38345cb0c0b39e9
maconidreamer <Laurean2279@yandex.ru>
2026-06-20 09:54:42 +0300

    Add MusicalShow class with libretto and music author

--- MusicalShow.java
+++ MusicalShow.java
@@ -1,1 +1,32 @@
+import java.util.ArrayList;
 
+public class MusicalShow extends Show {
+    private final String musicAuthor;
+    private final String librettoText;
+
+    public MusicalShow(
+            String title,
+            int duration,
+            Director director,
+            ArrayList<Actor> listOfActors,
+            String musicAuthor,
+            String librettoText
+    ) {
+        super(title, duration, director, listOfActors);
+        this.musicAuthor = musicAuthor;
+        this.librettoText = librettoText;
+    }
+
+    public String getMusicAuthor() {
+        return musicAuthor;
+    }
+
+    public String getLibrettoText() {
+        return librettoText;
+    }
+
+    public void printLibretto() {
+        System.out.println("Текст либретто спектакля \"" + getTitle() + "\":");
+        System.out.println(librettoText);
+    }
+}
