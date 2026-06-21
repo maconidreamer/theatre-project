8852409313ebb85f9b840541a12dd642c1c4608d
maconidreamer <Laurean2279@yandex.ru>
2026-06-21 07:02:03 +0300

    Fix toString method format in Actor class

--- Actor.java
+++ Actor.java
@@ -10,7 +10,7 @@ public class Actor extends Person {
 
     @Override
     public String toString() {
-        return super.toString() + ", рост: " + height;
+        return super.toString() + " (" + height + ")";
     }
 
     @Override
